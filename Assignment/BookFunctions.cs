7a5206040f513f88b677f2b635a0d9b8e43c192f
ayanada739 <ayanada739@gmail.com>
2024-08-05 21:59:42 +0300

    Solving Part 01 Q 01

---
+++ Assignment/BookFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class BookFunctions
+    {
+       
+            public static string GetTitle(Book B)
+            {
+                return B.Title;
+            }
+
+            public static string GetAuthors(Book B)
+            {
+                return string.Join(" , ", B.Authers);
+            }
+
+            public static string GetPrice(Book B)
+            {
+                return B.Price.ToString("C");
+            }
+        
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment
+{
+   
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Part 01
+            #region 1.  Considering the Code Below, Write Down the Body of all Listed Methods and Properties and Constructor:
+            //Class Book 
+            //Class BookFunctions
+
+            List<Book> books = new List<Book>
+            {
+            new Book("1234316", "Book Title 1", new string[] { "Auther 1" }, new DateTime(2023, 1, 1), 40.99m),
+            new Book("1234123" , "Book Title 2", new string[] { "Auther 2", "Author 3" }, new DateTime(2024, 5, 12), 50m)
+            };
+
+
+            #endregion
+
+
+        }
+    }
+}
