4abaabf251fc14b488a275a89a251ac8e8c128ef
ayanada739 <ayanada739@gmail.com>
2024-08-05 16:32:16 +0300

    Delegate

--- ADV 03/Program.cs
+++ ADV 03/Program.cs
@@ -1,10 +1,62 @@
 ﻿namespace ADV_03
 {
+    //Step 0 : Declare Delegate
+    public delegate int StringFuncDelegate(string str);
+    // New Class [Delegate]
+    // Any Reference From This Delegate Can Refer TO
+    // Function Or More [Pointer To Function)
+    // This Function May be Static Function [Class Member Function]
+    // This Function May be Non Static Function [Object Member Function]
+    // But These Functions Must be with the Same Signature of The Delegate
+    // Regardless Function Name & Access Modifier
+
+
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 07 Delegate
+            //// Delegate is a C# Lanaguge Feature
+            //// has 2 Usages
+            //// 1. Pointer To Function
+            //// 2. Event — Driven Programming
+
+            //int X = 5;
+
+            ////int Y =  StringFunctions.GetCountOfUpperChars; //Invalid
+
+            ////Step 01 Declare Reference From Delegate
+            //StringFuncDelegate stringFuncDelegate;
+
+            ////Step 02 Intialize Reference From Delegate
+            //stringFuncDelegate = new StringFuncDelegate(StringFunctions.GetCountOfUpperChars);
+            ////Syntax Suger
+            //stringFuncDelegate = StringFunctions.GetCountOfUpperChars;
+
+            ////Step 03 Use The Delegate Reference [Call Method ]
+            //int Result =  stringFuncDelegate.Invoke("OmAr Alaa");
+            //Console.WriteLine(Result);//3
+
+
+            ////stringFuncDelegate = StringFunctions.GetCountOfLowerChars;
+            ////stringFuncDelegate ==> GetCountOfLowerChars Only
+
+            //stringFuncDelegate += StringFunctions.GetCountOfLowerChars;
+            ////stringFuncDelegate ==> GetCountOfUpperChars + GetCountOfLowerChars
+
+            // //Result = stringFuncDelegate.Invoke("OmAr Alaa");
+            //// Console.WriteLine(Result);//5
+
+            //stringFuncDelegate -= StringFunctions.GetCountOfLowerChars;
+            ////stringFuncDelegate ==> GetCountOfUpperChars Only
+
+            //Result = stringFuncDelegate.Invoke("OmAr Alaa");
+            //Console.WriteLine(Result);//3
+
+            #endregion
+
+
         }
     }
 }
---
+++ ADV 03/StringFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADV_03
+{
+    internal class StringFunctions
+    {
+        public static int GetCountOfUpperChars(string Name)
+        {
+            int Count = 0;
+            if (Name is not null)
+                for (int i = 0; i < Name.Length; i++)
+                    if (char .IsUpper(Name[i]))
+                       Count++;
+            return Count;
+
+        }
+
+        public static int GetCountOfLowerChars(string Name)
+        {
+            int Count = 0;
+            if (Name is not null)
+                for (int i = 0; i < Name.Length; i++)
+                    if (char.IsLower(Name[i]))
+                        Count++;
+            return Count;
+        }
+}
